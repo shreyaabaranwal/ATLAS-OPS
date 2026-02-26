bb88a10f8745ab82d36cbf59bfb86174c07422bf
shreya229508 <shreyabaranwal229@gmail.com>
2026-02-26 21:27:50 +0530

    updated the code with details of ec2 configurations like , id, type, state,public or private ip, launch time and tag

--- atlas-ops/main.go
+++ atlas-ops/main.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
-	"github.com/aws/aws-sdk-go-v2/service/iam"
+
 )
 
 func main() {
@@ -26,23 +26,64 @@ if err != nil {
 	log.Fatalf("unable to describe instances, %v", err)
 }
 
-fmt.Println("EC2 instances:")
+fmt.Println("========== EC2 INSTANCES ==========")
+
+if len(result.Reservations) == 0 {
+	fmt.Println("No EC2 instances found.")
+	return
+}
 
 for _, reservation := range result.Reservations {
-for +, instance  := range reservation.Instances {
-	fmt.Println("Instance ID:", *instance.InstanceId)
-	fmt.Println("Instance Type:", instance.InstanceType)
-	fmt.Println("State:", instance.State.Name)
-	fmt.Println("Public IP:", *instance.PublicIpAddress)
-	fmt.Println("Private IP:", *instance.PrivateIpAddress)
-	fmt.Println("Launch Time:", instance.LaunchTime)
-	fmt.Println("Tags:")
-	for _, tag := range instance.Tags {
-		fmt.Printf("  %s: %s\n", *tag.Key, *tag.Value)
+	for _, instance := range reservation.Instances {
+
+		fmt.Println("-------------------------------------------------")
+
+		// Instance ID
+		if instance.InstanceId != nil {
+			fmt.Println("Instance ID      :", *instance.InstanceId)
+		}
+
+		// Instance Type
+		fmt.Println("Instance Type    :", instance.InstanceType)
+
+		// State
+		if instance.State != nil {
+			fmt.Println("State            :", instance.State.Name)
+		}
+
+		// Public IP
+		if instance.PublicIpAddress != nil {
+			fmt.Println("Public IP        :", *instance.PublicIpAddress)
+		} else {
+			fmt.Println("Public IP        : N/A")
+		}
+
+		// Private IP
+		if instance.PrivateIpAddress != nil {
+			fmt.Println("Private IP       :", *instance.PrivateIpAddress)
+		} else {
+			fmt.Println("Private IP       : N/A")
+		}
+
+		// Launch Time
+		if instance.LaunchTime != nil {
+			fmt.Println("Launch Time      :", instance.LaunchTime)
+		}
+
+		// Tags
+		fmt.Println("Tags             :")
+		if len(instance.Tags) == 0 {
+			fmt.Println("  No Tags")
+		} else {
+			for _, tag := range instance.Tags {
+				if tag.Key != nil && tag.Value != nil {
+					fmt.Printf("  %s : %s\n", *tag.Key, *tag.Value)
+				}
+			}
+		}
+
+		fmt.Println("-------------------------------------------------")
+		fmt.Println()
 	}
-	fmt.Println()
 }
-}
-
-
 }
