4a0b2bfca0e4953716b10f5d1a02baca69f0a07d
shreya229508 <shreyabaranwal229@gmail.com>
2026-02-27 10:30:35 +0530

    writing logic for policy/engine

---
+++ atlas-ops/policy/engine.go
@@ -0,0 +1,46 @@
+package policy
+type Recommendation struct {
+	Status   string  `json:"status"`
+	CPU      float64  `json:"cpu"`
+	InstanceType string `json:"instance_type"`
+	Recommendation string `json:"recommendation"`
+	
+}
+
+
+func EvaluatePolicy(cpu float64, instanceType string) Recommendation {
+	rec := Recommendation{
+		CPU:     cpu,
+        InstanceType: instanceType,
+	}
+
+
+	if cpu > 80 {
+		rec.Status = " HIGH_CPU"
+
+		switch instanceType {
+		case "t2.micro":
+			rec.Recommendation = "Upgrade to t3.medium"
+		case "t3.medium":
+			rec.Recommendation = "Upgrade to t3.Large"
+		default:
+			rec.Recommendation = "Consider vertical scaling "
+
+		}
+		return rec
+	
+	
+	}
+if cpu > 50 {
+	rec.Status = "Moderate Load"
+	rec.Recommendation = "Monitor closely"
+	return rec 
+
+}
+
+rec.Status = "HEALTHY "
+rec.Recommendation = "No action needed"
+return rec 
+
+
+}
