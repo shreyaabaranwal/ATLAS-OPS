d47cada8bc9e0c026d108c03e16b0d52dd5ef4c5
shreyaabaranwal <shreyabaranwal229@gmail.com>
2026-05-09 06:31:41 +0530

    adding ec2 layer and security group access

---
+++ Teraform/ec2.tf
@@ -0,0 +1,27 @@
+resource "aws_security_group "atlas_sg" {
+    name = ${var.project_name}-sg"
+
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+ ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+ egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+
+
+}
