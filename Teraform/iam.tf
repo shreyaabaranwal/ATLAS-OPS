356f91e20fcc673a9773cbaf8a0270513c9d6e0e
shreyaabaranwal <shreyabaranwal229@gmail.com>
2026-05-09 05:53:46 +0530

    adding iam layer to give permission to other services

---
+++ Teraform/iam.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_role "atlas_ec2_role" {
+    name = "${var.project_name}-ec2-role"
+
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17"
+
+        Statement = [
+            {
+                Effect = "Allow"
+                Principal = {
+
+                Service = "ec2.amazonaws.com"
+            } 
+            Action = "sts:AssumeRole"
+            }
+
+        ]
+    })
+}
